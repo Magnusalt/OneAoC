8b5745aea28629ed5ffa99fbcdf4c300dc8033b2
Magnus Altgard <magnus.altgard@gmail.com>
2025-12-08 09:38:37 +0100

    Day 5 tests

---
+++ Tests/AoC2025Tests/Day05Tests.cs
@@ -0,0 +1,25 @@
+﻿using AoC2025;
+
+namespace AoC2025Tests;
+
+[TestClass]
+public class Day05Tests
+{
+    private readonly Day05 _sut = new();
+
+    [TestMethod]
+    public void Part1_Example()
+    {
+        var input = File.ReadAllLines("../../../../../Years/AoC2025/example/day05.txt");
+        var result = _sut.RunPart1(input);
+        Assert.AreEqual("3", result);
+    }
+
+    [TestMethod]
+    public void Part2_Example()
+    {
+        var input = File.ReadAllLines("../../../../../Years/AoC2025/example/day05.txt");
+        var result = _sut.RunPart2(input);
+        Assert.AreEqual("14", result);
+    }
+}
